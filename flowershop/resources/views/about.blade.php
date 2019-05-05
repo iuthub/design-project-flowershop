1950cf93580b53b54d5a1f180fc6e9bb579ad0ee
JakhongirAlimov <jahonwpp@gmail.com>
2019-05-05 21:53:25 +0500

    Changes Updated

---
+++ flowershop/resources/views/about.blade.php
@@ -0,0 +1,148 @@
+@extends('layouts.master')
+  
+@section('content')
+    @include('partials.errors')
+<section id="aa-catg-head-banner">
+   <img src="img/slider.jpg" style="height: 200px; width: 100%" alt="fashion img">
+   <div class="aa-catg-head-banner-area">
+     <div class="container">
+      <div class="aa-catg-head-banner-content">
+        <h2>About</h2>
+        
+      </div>
+     </div>
+   </div>
+  </section>
+  
+
+  <section id="aa-blog-archive">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="aa-blog-archive-area">
+            <div class="row">
+              <div class="col-md-9">
+
+                <div class="aa-blog-content aa-blog-details">
+                  <article class="aa-blog-content-single">                        
+                    <h2><a href="#">#1 Online Flower Delivery Portal </a></h2>
+                    
+                    
+                    <p>Is there anyone in this world who does not like flowers? Anyone who does not admire the beauty of flowers? Of course not! Because flowers undoubtedly bring a smile on everyone's face, they add the warmth of sunshine to the day and make the soul feel calm and peaceful. And so to speak the language of flowers and express emotions when words are just not enough, FlowerAura has taken the responsibility to become your messenger of Emotions, Send flower bouquet online and add more smiles to your loved one's face.
+
+But finding a beautiful bunch of flower that speaks your emotions perfectly might be a tough task. That is when FlowerAura steps in with amazing flower gifts of amazing varieties so that you don't have to hop to florist shops around you and be settled with a limited choice. FlowerAura shall be your reliable online florist shop in India so that you can sit at the comfort of your home and order amazing flowers and gifts with a few taps of your finger.
+
+So, what is their favorite flower? Does your recipient love Roses, Carnations, Gerberas, Anthuriums, orchids, Lilies or? Whichever be the flower of their choice, find it at the best online florist in India- FlowerAura. We deliver freshly cut flowers in the most beautiful and classic arrangements. Wrapped beautifully in different designs and colorful paper, these flower arrangements are the best quality products which shall make your loved one's face shine even brighter.
+
+So the next time you need to send flowers online in India, you know where to head to.</p>
+                    <p>lowerAura stands as a No. 1 online flower shop that doesn't only deliver flowers, gifts, and cakes but makes sure that each of the hampers is wrapped in love too. Along with the pocket-friendly deals come the free shipping that makes FlowerAura top the charts and establishing itself as one of the most reliable online florists in the country.</p>
+                    <blockquote>
+                      Perfect Fresh Flowers Gifts for Your Loved Ones from Online Flower Shop
+                    </blockquote>
+                    <p>A Mom's role is incomparable and incomprehensible. All that she bears, does and collects herself to perform each day with perfection is worth an applause. Tell your mom that her strength and perseverance go beyond that of superheroes. Thank her for being an all-rounder. You might not always be able to tell your Mom how much you love her for everything she does. But there are certain ways that express your feelings in the best way. FlowerAura’s scintillating collection of Mother’s Day flowers and gifts is sure to let her know that no matter how near or far you are, you shall never stop caring for her.
+
+FlowerAura has a vast option of Mother’s Day gifts to choose from. From scintillating flowers to breathtaking hampers, delicious cake to handmade chocolates, make a pick of the most amazing Mother’s day gifts and delight your mom like never before. We have specially designed flowers for mothers, personalized Mother’s day cakes like photo cakes, various flavour cakes, personalized mom cushions etc.
+
+Even if your mom and you are miles apart, you can still thank her with your special gestures. FlowerAura shall make the home delivery of your Mother’s Day gift hampers that comes along with free shipping. Each of our gifts are lovingly hand crafted to make your expression of love unique. Therefore, whether you are seeking special gifts for your mom or some thoughtful know, you know where to go. </p>
+                    
+                    
+                  </article>
+             
+                  <div class="aa-blog-comment-threat">
+                    <h3>If any Comments and Suggestions</h3>
+                    <div class="comments">
+                      <ul class="commentlist">
+                    
+                       
+                        
+                         
+                        <li>
+                          <div class="media">
+                            <div class="media-left">    
+                                <img class="media-object news-img" src="img/testimonial-img-2.jpg" alt="img">      
+                            </div>
+                            <div class="media-body">
+                             <h4 class="author-name">Aziz Bokhidjonov</h4>
+                             <span class="comments-date"> Date of Post</span>
+                             <p>Your delivery service is very perfect!!! Good Luck...</p>
+                             
+                            </div>
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="aa-blog-archive-pagination">
+                      <nav>
+                        <ul class="pagination">
+                          <li>
+                            <a href="#" aria-label="Previous">
+                              <span aria-hidden="true">«</span>
+                            </a>
+                          </li>
+                          <li><a href="#">1</a></li>
+                          <li><a href="#">2</a></li>
+                          <li><a href="#">3</a></li>
+                          <li><a href="#">4</a></li>
+                          <li><a href="#">5</a></li>
+                          <li>
+                            <a href="#" aria-label="Next">
+                              <span aria-hidden="true">»</span>
+                            </a>
+                          </li>
+                        </ul>
+                      </nav>
+                    </div>
+                  </div>
+         
+                  <div id="respond">
+                    <h3 class="reply-title">Leave a Comment</h3>
+                    <form id="commentform">
+                      
+                      <p class="comment-form-author">
+                        <label for="author">Name <span class="required">*</span></label>
+                        <input type="text" name="author" value="" size="30" required="required">
+                      </p>
+                      <p class="comment-form-email">
+                        <label for="email">Email <span class="required">*</span></label>
+                        <input type="email" name="email" value="" aria-required="true" required="required">
+                      </p>
+                      
+                      <p class="comment-form-comment">
+                        <label for="comment">Comment</label>
+                        <textarea name="comment" cols="45" rows="8" aria-required="true" required="required"></textarea>
+                      </p>
+                      
+                      <p class="form-submit">
+                        <input type="submit" name="submit" class="aa-browse-btn" value="Post">
+                      </p>        
+                    </form>
+                  </div>
+                </div>
+              </div>
+
+            
+              <div class="col-md-3">
+                <aside class="aa-blog-sidebar">
+                  <div class="aa-sidebar-widget">
+                    <h3>Category</h3>
+                    <ul class="aa-catg-nav">
+                      <li><a href="#">Flower</a></li>
+                      <li><a href="">Plants</a></li>
+                      <li><a href="">Occasion</a></li>
+                      <li><a href="">Cake</a></li>
+                      <li><a href="">Gifts</a></li>
+                      <li><a href="">Blogs</a></li>
+                     <li><a href="">Personalaized</a></li>
+
+                    </ul>
+                  </div>
+                  
+                </aside>
+              </div>
+            </div>           
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+   @endsection
